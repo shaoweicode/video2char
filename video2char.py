e5552e5a12bb1dd765fabfef080b7a3a4180bb2b
shaoweicode <417096554@qq.com>
2017-12-03 21:52:42 +0800

    没能实现转为视频

--- video2char.py
+++ video2char.py
@@ -1,7 +1,34 @@
 import cv2
 import matplotlib.image as mpimg
 import numpy as np
-from moviepy.editor import VideoFileClip
+#from moviepy.editor import VideoFileClip
 import imageio
 
 
+img = cv2.imread('/home/python/video2char/test2.jpg',3)
+codelib = '''@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. '''
+
+def image_process(image):
+    txt=''
+    im_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    [m,n]= im_gray.shape  
+    h=int(m/4)
+    w=int(n/4)
+    im_graymini = cv2.resize(im_gray,(h,w))
+
+    length = len(codelib)
+    unit = 257.0/length
+    for i in range(h):
+        for j in range(w):
+            txt += codelib[int(im_graymini[j,i]/unit)]
+        txt += '\n'
+    return im_graymini,txt
+    
+
+
+    
+
+
+
+
+mini,charmap = image_process(img)
